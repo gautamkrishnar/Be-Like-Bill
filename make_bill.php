2c7dc3306f4703b5c2e2428b31d09d28c72b1c18
gautamkrishnar <r.gautamkrishna@gmail.com>
2016-01-30 01:33:25 +0530

    fix mobile

--- make_bill.php
+++ make_bill.php
@@ -4,9 +4,6 @@
         <link rel="shortcut icon" href="icon.png" />
         
         <meta charset="UTF-8">
-        <meta name="description" content="Be like Bill is an online platform that allows you to create your own "Be like Bill" Memes. Its really simple and easy to use. It also has an API to create and use Be like Bill memes on your website.">
-        <meta name="keywords" content="">
-        <meta name="author" content="Gautam krishna R">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut" src="icon.png" />
         <link rel="stylesheet" type="text/css" href="style.css">
@@ -18,14 +15,14 @@
         if(filter_input(INPUT_POST, 'name')){
         $name = filter_input(INPUT_POST, 'name');
         $sex = filter_input(INPUT_POST, 'sex');
-        echo '<img src="billgen.php?name='.$name.'&sex='.$sex.'" />';
+        echo '<img src="billgen.php?name='.$name.'&sex='.$sex.'" class="billimg" />';
         }
  else {
-     echo '<img src="billgen.php" />';
+     echo '<img src="billgen.php" class="billimg"/>';
  }
          ?>
         <br/><br/>
-        Right click the image to and select the "save as" option to save.
+        Right click the image to and select the "save image as" option to save.
         <br/><br/><b>
         Created with <img src="http://www.clker.com/cliparts/V/a/r/B/D/o/love-md.png" width="15px" height="inherit"/> 
         By <a href="http://github.com/gautamkrishnar/" target="_blank">Gautam krishna R.</a><br/>
